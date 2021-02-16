bcd0eb796ba845e0d7fbb579b160c3cf95bbfde0
unknown <suyashgaikwad31@gmail.com>
2021-02-16 18:17:52 +0530

    added flipcoin.sh

---
+++ flipcoin.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+i=0
+countH=0
+countT=0
+diff=0
+while [ $countH -lt 21 -a $countT -lt 21 -o $diff -lt 5 -a $diff -gt -5 ]
+do
+        coin=$(( RANDOM%2 ))
+        if [ $coin -eq 1 ]
+        then
+                countH=$(( countH+1 ))
+        else
+                countT=$(( countT+1 ))
+        fi
+        i=$(( i+1 ))
+        diff=$(( countH-countT ))
+done
+
+
